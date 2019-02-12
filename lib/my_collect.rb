b83b16e15e125dfc5726fce61c557fc815b933dd
Blaine Lawson <blaine.lawson@gmail.com>
2019-02-12 15:53:04 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,21 @@
+names = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
+def my_collect(array)
+  if block_given?
+    i = 0
+    
+    while i < array.length
+      yield(array[i])
+      i += 1
+    end
+    array
+  else
+    "Block not provided"
+  end
+end
 
+my_collect(names) do |name|
+  name.split(" ")
+end
+
+def outside_the_box(["Patricia Dunn", "Albert Pumpernickelk"])
